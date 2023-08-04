1fb8895431ee5d5ff29f0a743a81472246867184
Pranshav Vikram <0201cs046@niet.co.in>
2023-08-04 20:02:18 +0530

    card

--- src/component/Card.js
+++ src/component/Card.js
@@ -7,7 +7,7 @@ const Card = () => {
         width: "185px",
         height: "180px",
         padding: "8px",
-        backgroundColor: "blue",
+        backgroundColor: "lightpink",
         boxShadow: "2px 2px grey",
         borderRadius: "0.375rem",
         
@@ -16,7 +16,6 @@ const Card = () => {
       <h4>Title</h4>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum omnis
-        repellendus nesciunt culpa error modi.
       </p>
     </div>
   );
